548d38a88038c203691199a24aaa33cf98f1e583
eldar <eldar.mustafa@speedandfunction.com>
2018-07-27 16:03:06 +0300

    updated home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,8 @@ module ApplicationHelper
     base_title = "Ruby on Rails sample app"
     if page_title.empty?
       base_title
+    else
+      page_title + " | " + base_title
     end
-  else
-    page_title + " | " + base_title
   end
 end
