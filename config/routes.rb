4f134113aa188d2af81dcb5ea561e66fad87a0d3
eldar <eldar.mustafa@speedandfunction.com>
2018-08-02 07:54:43 +0300

    implemented basic login

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,12 @@ Rails.application.routes.draw do
 
   get '/try_haml', to: 'static_pages#try_haml'
 
+  get '/login', to: 'sessions#new'
+
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
